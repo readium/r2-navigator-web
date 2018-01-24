0dcf6d4412739f66ed557829b1c87ffa0b733cb2
Juan Corona <juanc@evidentpoint.com>
2018-01-23 18:17:44 -0800

    Improve test config

--- webpack-test.config.js
+++ webpack-test.config.js
@@ -1,7 +1,9 @@
+const glob = require("glob");
+
 module.exports = {
     context: __dirname,
     devtool: "inline-source-map",
-    entry: "./build_test_temp/test/streamer-client-test.js",
+    entry: glob.sync('./build_test_temp/test/*.js'),
     output: {
       path: __dirname + "/build_test_temp",
       filename: "test_bundle.js"
