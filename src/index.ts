cdff0b076d923e5b82ce353a6485164332c090c6
Juan Corona <juanc@evidentpoint.com>
2018-02-09 12:51:16 -0800

    Attempt at fixing typings generation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import { readFileSync } from 'fs';
-
-import { StreamerClient } from './navigator/streamer-client';
-
-const sc = new StreamerClient();
-
-const webpub = readFileSync('/Users/lilid/Documents/dev/ATB/bugs/manifest.json', 'utf8');
-const pub = sc.openPublicationFromJson(webpub);
-console.log(pub.Context);
+export * from './navigator';
