89291fd1282df5a4ed817e611529df1833e4bedc
Lili Deng <lilid@evidentpoint.com>
2018-08-22 15:48:02 -0700

    Remove compat layer code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
 export * from './streamer/publication';
 export * from './navigator';
-export * from './readium-shared-js-export';
-export * from './compat/reader-view-adapter';
-export * from './compat/readium';
--- src/navigator/index.ts
+++ src/navigator/index.ts
@@ -6,3 +6,4 @@ export { Viewport } from './views/viewport';
 export { LayoutView } from './views/layout-view';
 export { CancellationToken, ZoomOptions } from './views/types';
 export { getReadiumEventsRelayInstance } from './views/readium-events-relay';
+export { NavigationRequestManager } from './request-manager';
