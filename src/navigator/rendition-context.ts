c879c77051d82fd0910590af341e7da313456ce1
Juan Corona <juan.carlos.corona.r@gmail.com>
2019-05-13 14:50:01 -0700

    Remove usages of glue js and related

--- src/navigator/rendition-context.ts
+++ src/navigator/rendition-context.ts
@@ -1,4 +1,3 @@
-import { GlueManager } from './glue-manager';
 import { IFrameLoader } from './iframe-loader';
 import { Navigator } from './navigator';
 import { Rendition } from './rendition';
@@ -9,13 +8,11 @@ export class RenditionContext {
   public rendition: Rendition;
   public navigator: Navigator;
   private iframeLoader: IFrameLoader;
-  private glueManager: GlueManager;
 
   constructor(rendition: Rendition, iframeLoader: IFrameLoader) {
     this.rendition = rendition;
     this.iframeLoader = iframeLoader;
     this.requestManager = new NavigationRequestManager();
     this.navigator = new Navigator(this.rendition, this.requestManager);
-    this.glueManager = new GlueManager(this, iframeLoader);
   }
 }
