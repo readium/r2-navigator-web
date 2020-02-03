ed4b7f9e8ae6e6820813f08ddaabb82c8465e62f
Juan Carlos Corona Romero <juanc@evidentpoint.com>
2020-02-03 15:51:31 -0800

    Export all in utils

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * from './streamer/publication';
 export * from './navigator';
-export { PageTitleTocResolver } from './utils/page-title-toc-resolver';
-export { Resource } from './utils/injection-resolver';
-export { ViewportResizer } from './utils/viewport-resizer';
+export * from './utils';
