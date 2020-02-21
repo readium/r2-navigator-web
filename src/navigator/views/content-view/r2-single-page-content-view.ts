34886c455d0761febc8b71cf8e95004306163bee
Michael Liu <mikeliu@evidentpoint.com>
2020-02-20 16:46:39 -0800

    Reverted unnecessary changes in file

--- src/navigator/views/content-view/r2-single-page-content-view.ts
+++ src/navigator/views/content-view/r2-single-page-content-view.ts
@@ -118,7 +118,6 @@ export class R2SinglePageContentView extends R2ContentView  {
 
     const cfi = this.cfiNavLogic.getFirstVisibleCfi(new Rect(left, top, right, bottom), backward);
 
-
     return cfi ? cfi : '';
   }
 
